0c93bfbc4743bfee8d4431f130a2894f9479e667
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-02-20 19:02:26 -0300

    add: components display name, controls and addon-a11y

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/saturdayintokyo.png',
     alt: 'SaturdayInTokyo',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
